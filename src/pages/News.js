433f03f49e31f0c8c10aef0d8f9c34c2d559c229
tyk <110590015@ntut.org.tw>
2023-05-29 13:42:02 +0800

    news and results added

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,7 +1,32 @@
 import { Col, Row, Button } from "react-bootstrap";
+import { news } from "../data/news_data";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '../css/style.css'
+import '../css/map.css'
 
 export default function News() {
     return (
-        <div></div>
+        <div className="c_container">
+            <div className="content">
+                <h2 className="blue_word">最新消息</h2>
+                <br />
+                暫無資料
+                {/* {news.map((data) => {
+                    return (
+                        <Row>
+                            <Col sm={12} md={3}>
+                                {data.year}/{data.month}/{data.date}
+                            </Col>
+                            <Col sm={12} md={9}>
+                                <a href={"/news/" + data["news_id"]}>{data.title}</a>
+                                <hr />
+                            </Col>
+                        </Row>
+                    )
+                })} */}
+            </div>
+        </div>
     )
 }
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,7 +1,32 @@
 import { Col, Row, Button } from "react-bootstrap";
+import { result } from "../data/result_data";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '../css/style.css'
+import '../css/map.css'
 
 export default function Result() {
     return (
-        <div></div>
+        <div className="c_container">
+            <div className="content">
+                <h2 className="blue_word">計畫成果</h2>
+                <br />
+                暫無資料
+                {/* {result.map((data) => {
+                    return (
+                        <Row>
+                            <Col sm={12} md={3}>
+                                {data.year}/{data.month}/{data.date}
+                            </Col>
+                            <Col sm={12} md={9}>
+                                <a href={"/result/" + data["result_id"]}>{data.title}</a>
+                                <hr />
+                            </Col>
+                        </Row>
+                    )
+                })} */}
+            </div>
+        </div>
     )
 }
