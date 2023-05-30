b86b19e7612b56bd42b605beddf44a799a237740
tyk <110590015@ntut.org.tw>
2023-05-30 21:57:15 +0800

    course and courseEach wig

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -1,7 +1,143 @@
 import { Col, Row, Button } from "react-bootstrap";
+import { course_map } from '../data/map_data'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComputer, faMicrochip, faGlobe, faLayerGroup, faPeopleGroup, faMousePointer } from "@fortawesome/free-solid-svg-icons";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '../css/style.css'
+import '../css/map.css'
 
 export default function Course() {
+
     return (
-        <div></div>
+        <div className="c_container">
+            <div className="content">
+                <h2 className="blue_word">
+                    知識地圖
+                </h2>
+
+                <h5 className="orange_word"><b>核心能力</b></h5>
+                <p className="inside">
+                    本計畫訂定了系統軟體與韌體人才所需之五大核心能力如下：
+                    <br />
+                    <b className="con_color"><FontAwesomeIcon icon={faComputer} /> &nbsp;作業系統核心能力：</b>
+                    <br />瞭解作業系統之基礎概念與其在系統軟體中之角色，以及作業系統的核心概念，並掌握相關技術開發之能力。
+                    <br />
+                    <b className="kno_color"><FontAwesomeIcon icon={faMicrochip} /> &nbsp;硬體架構與設計能力：</b>
+                    <br />瞭解常見硬體之協定內容、硬體描述語言之定義，並具備硬體規範之閱讀能力。
+                    <br />
+                    <b className="tec_color"><FontAwesomeIcon icon={faGlobe} /> &nbsp;網路程式設計能力：</b>
+                    <br />撰寫程式語言或應用軟體進行網路連接，並且熟悉不同型態的網路協定，以及如何使用網路程式設計進行系統軟體中所需之連線實作。
+                    <br />
+                    <b className="val_color"><FontAwesomeIcon icon={faLayerGroup} /> &nbsp;開源工具之熟悉與使用能力：</b>
+                    <br />使用適當的開源軟體工具解決產業相關問題，並使用Git與GitHub進行版本管理以及使用Jenkins 進行持續整合測試。
+                    <br />
+                    <b className="val_color"><FontAwesomeIcon icon={faPeopleGroup} /> &nbsp;團隊合作與溝通協調能力：</b>
+                    <br />願意與不同專長背景同學交換知識、想法、價值觀等資訊，並實現跨領域小組中討論核心問題。
+                </p>
+
+                <div>
+                    {course_map.map((data) => {
+                        return (
+                            <Row>
+                                <Col sm={12} md={2} >
+                                    <h5><b>{data.category}</b></h5>
+                                    {data.depiction}
+                                </Col>
+                                <Col sm={12} md={10}>
+                                    <hr />
+                                </Col>
+                                <Col sm={12} md={12} className="inside">
+                                    {data.inner_data.map((datas) => {
+                                        var os = datas.os,
+                                            structure = datas.structure,
+                                            web_program = datas.web_program,
+                                            open_source = datas.open_source,
+                                            team = datas.team;
+
+                                        os = Os(os);
+                                        structure = Stru(structure);
+                                        web_program = WebPro(web_program);
+                                        open_source = OpenSour(open_source);
+                                        team = Team(team);
+
+                                        function Os(os) {
+                                            if (os === "") {
+                                                return <div> &nbsp;&nbsp;&nbsp;</div>
+                                            }
+                                            return <b className="con_color"><FontAwesomeIcon icon={faComputer} /></b>
+                                        }
+
+                                        function Stru(structure) {
+                                            if (structure === "") {
+                                                return <div> &nbsp;&nbsp;&nbsp;</div>
+                                            }
+                                            return <b className="kno_color"><FontAwesomeIcon icon={faMicrochip} /></b>
+                                        }
+
+                                        function WebPro(web_program) {
+                                            if (web_program === "") {
+                                                return <div> &nbsp;&nbsp;&nbsp;</div>
+                                            }
+                                            return <b className="tec_color"><FontAwesomeIcon icon={faGlobe} /></b>
+                                        }
+
+                                        function OpenSour(open_source) {
+                                            if (open_source === "") {
+                                                return <div> &nbsp;&nbsp;&nbsp;</div>
+                                            }
+                                            return <b className="val_color"><FontAwesomeIcon icon={faLayerGroup} /></b>
+                                        }
+
+                                        function Team(team) {
+                                            if (team === "") {
+                                                return <div> &nbsp;&nbsp;&nbsp;</div>
+                                            }
+                                            return <b className="val_color"><FontAwesomeIcon icon={faPeopleGroup} /></b>
+
+                                        }
+
+                                        return (
+                                            <Row>
+                                                <Col sm={12} md={9} className='map_inside'>
+                                                    < Row >
+                                                        <Col sm={6} className='map_inside'>
+                                                            <a href={"/map/course/" + datas["course_id"]} className='map_a'>
+                                                                <div className="map_btn">
+                                                                    <b>{datas.course_name}</b> &nbsp; <FontAwesomeIcon icon={faMousePointer} />
+                                                                </div>
+                                                            </a>
+                                                        </Col>
+                                                        <Col xs={2} className='center'>
+                                                            <b>{datas.credit}學分</b>
+                                                        </Col>
+                                                        <Col xs={4} className='center inside'>
+                                                            <div className="">
+                                                                <b>{datas.course_time[0].semester}</b>
+                                                            </div>
+                                                        </Col>
+                                                    </Row>
+                                                </Col>
+                                                <Col sm={12} md={3} className=' center'>
+                                                    <Row className="center ">
+                                                        <Col xs={2} className=" dot">{os}</Col>
+                                                        <Col xs={2} className=" dot">{structure}</Col>
+                                                        <Col xs={2} className=" dot">{web_program}</Col>
+                                                        <Col xs={2} className=" dot">{open_source}</Col>
+                                                        <Col xs={2} className=" dot">{team}</Col>
+                                                    </Row>
+                                                </Col>
+                                            </Row>
+                                        );
+                                    })}
+                                </Col>
+                            </Row >
+                        );
+                    })}
+                </div >
+            </div >
+        </div >
     )
 }
--- src/pages/CourseEach.js
+++ src/pages/CourseEach.js
@@ -1,7 +1,224 @@
 import { Col, Row, Button } from "react-bootstrap";
+import { course_map } from '../data/map_data'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '../css/style.css'
+import '../css/map.css'
 
 export default function CourseEach() {
+    const id = (window.location.href).slice(-2),
+        temp = findData(id),
+        category = temp[0],
+        data = temp[1],
+        os = OS(data.os),
+        structure = Stru(data.structure),
+        web_program = WebPro(data.web_program),
+        open_source = OpenSour(data.open_source),
+        team = Team(data.team);
+
+    function findData(id) {
+        for (var i = 0; i < course_map.length; i++) {
+            for (var j = 0; j < course_map[i].inner_data.length; j++) {
+                if (course_map[i].inner_data[j].course_id === id) {
+                    return [course_map[i].category, course_map[i].inner_data[j]];
+                }
+            }
+        }
+    }
+
+    function OS(os) {
+        if (os === "") {
+            return <div />
+        }
+        return <div>作業系統核心</div>
+    }
+
+    function Stru(structure) {
+        if (structure === "") {
+            return <div />
+        }
+        return <div>硬體架構與設計</div>
+    }
+
+    function WebPro(web_program) {
+        if (web_program === "") {
+            return <div />
+        }
+        return <div>網路程式設計</div>
+    }
+
+    function OpenSour(open_source) {
+        if (open_source === "") {
+            return <div />
+        }
+        return <div>開源工具之熟悉與使用</div>
+    }
+
+    function Team(team) {
+        if (team == "") {
+            return <div />
+        }
+        return <div>團隊合作與溝通協調</div>
+    }
+
+
     return (
-        <div></div>
+        < div className="course_container" >
+            <div className="course_bg">
+                <Row>
+                    <Col sm={3} className='center'>
+                        <h5>
+                            【{category}課程】
+                        </h5>
+                    </Col>
+                    <Col sm={9}>
+                        <h2>
+                            {data.course_name}
+                        </h2>
+                    </Col>
+                    <hr></hr>
+                    <Col sm={12} className='orange_word'>
+                        <p><b>{data.course_depiction}</b></p>
+                    </Col>
+                    <Col sm={12}>
+                        <p>{data.note}</p>
+                    </Col>
+                    <Col sm={2} className='light_green inside center'>
+                        核心能力
+                    </Col>
+                    <Col sm={10} className='green inside'>
+                        <Row>
+                            <Col sm={1} />
+                            <Col sm={2} className="center">
+                                {os}
+                            </Col>
+                            <Col sm={2} className="center">
+                                {structure}
+                            </Col>
+                            <Col sm={2} className="center">
+                                {web_program}
+                            </Col>
+                            <Col sm={2} className="center">
+                                {open_source}
+                            </Col>
+                            <Col sm={2} className="center">
+                                {team}
+                            </Col>
+                        </Row>
+                    </Col>
+
+                    <Col sm={4} className='inside'>
+                        開課單位
+                    </Col>
+                    <Col sm={8} className='inside'>
+                        {data.department}
+                    </Col>
+
+                    <Col sm={12} md={4}></Col>
+                    <Col sm={12} md={8}>
+                        <hr />
+                    </Col>
+
+                    <Col sm={4} className='inside'>
+                        授課教師
+                    </Col>
+                    <Col sm={8} className='inside'>
+                        {data.lecturer}
+                    </Col>
+
+                    <Col sm={12} md={4}></Col>
+                    <Col sm={12} md={8}>
+                        <hr />
+                    </Col>
+
+                    <Col sm={4} className='inside'>
+                        開課學期
+                    </Col>
+                    <Col sm={8} className='inside'>
+                        {data.course_time.map((time) => {
+                            return (
+                                <Row>
+                                    <Col sm={12}>
+                                        {time.semester}
+                                    </Col>
+                                </Row>
+                            );
+                        })}
+                    </Col>
+                    <Col sm={12} md={4}></Col>
+                    <Col sm={12} md={8}>
+                        <hr />
+                    </Col>
+
+
+                    <Col sm={4} className='inside'>
+                        課程時段
+                    </Col>
+                    <Col sm={8} className='inside'>
+                        {data.course_time.map((time) => {
+                            return (
+                                <Row>
+                                    <Col sm={6}>
+                                        課號：{time.id}
+                                    </Col>
+                                    <Col sm={6}>
+                                        {time.day} &nbsp; {time.time}
+                                    </Col>
+                                </Row>
+                            );
+                        })}
+                    </Col>
+                    <Col sm={12} md={4}></Col>
+                    <Col sm={12} md={8}>
+                        <hr />
+                    </Col>
+
+                    <Col sm={4} className='inside'>
+                        學分數
+                    </Col>
+                    <Col sm={8} className='inside'>
+                        {data.credit}學分，每週{data.credit}小時
+                    </Col>
+                    <Col sm={12} md={4}></Col>
+                    <Col sm={12} md={8}>
+                        <hr />
+                    </Col>
+
+                    <Col sm={4} className='inside'>
+                        課程概述
+                    </Col>
+                    <Col sm={8} className='inside'>
+                        <p>{data.course_info_ch}</p>
+                    </Col>
+                    <Col sm={12} md={4}></Col>
+                    <Col sm={12} md={8}>
+                        <hr />
+                    </Col>
+
+                    <Col sm={4} className='inside'>
+                        English Description
+                    </Col>
+                    <Col sm={8} className='inside'>
+                        <p>{data.course_info_en}</p>
+                    </Col>
+                    <Col sm={12} md={4}></Col>
+                    <Col sm={12} md={8}>
+                        <hr />
+                    </Col>
+
+                    <Col sm={12}>
+                        <br />
+                    </Col>
+                    <Col sm={12} className='center'>
+                        <a href={"/map"}>
+                            <Button className="blue">
+                                上一頁
+                            </Button>
+                        </a>
+                    </Col>
+                </Row>
+            </div>
+        </div >
     )
 }
